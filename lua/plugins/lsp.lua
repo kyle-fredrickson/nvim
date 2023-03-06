a33e8fcbe14a200e22a631f551e8904690233078
Kyle Fredrickson <kyfredri@ucsc.edu>
2023-03-06 15:13:13 -0800

    Fixed error in LSP.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,6 +5,7 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
   ensure_installed = {
+    "lua_ls",
     "clangd",
     "hls",
     "pylsp",
@@ -19,9 +20,13 @@ local on_attach = function(_, _)
 
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
   vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-  vim.keymap.set("n", "gr", require("telescope.builtin"), {})
+  vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, {})
   vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 end
+
+require("lspconfig").lua_ls.setup {
+  on_attach = on_attach
+}
 
 require("lspconfig").clangd.setup {
   on_attach = on_attach
